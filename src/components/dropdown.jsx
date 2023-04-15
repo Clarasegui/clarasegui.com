44bee0d74dc4ae92acca912517a076a48fa09d2a
Clara Segui <clarasegui@macbook-air.home>
2023-04-15 12:14:58 +0200

    Header and pages

---
+++ src/components/dropdown.jsx
@@ -0,0 +1,9 @@
+import '../styles/dropdown'
+
+function Dropdown(props) {
+    return(
+        <li className="dropdown_menu"><a>(props.text)</a></li>
+    );
+}
+
+export default Dropdown;
---
+++ src/components/header.jsx
@@ -0,0 +1,22 @@
+import '../styles/header.css';
+
+function Header() {
+    return (
+        <header>
+        <div className="title_container">
+            <h1>c l a r a s e g u i</h1>
+            </div>
+            <div className="nav_container">
+                <nav>
+                    <li>personnal work</li>
+                    <li>commissioned</li>
+                    <li>editorial</li>
+                    <li>diary</li>
+                    <li>contact</li>
+                </nav>
+            </div>
+            </header>
+    );
+}
+
+export default Header;
